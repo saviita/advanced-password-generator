e2b18eb5fb4630fc45300198fc144e4f9ab1bed5
saviita <taatt2000@gmail.com>
2024-06-25 11:25:20 +0200

    25/6

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,6 @@ const lengthElement = document.getElementById('length');
 const rangeElement = document.getElementById('range');
 const btnElement = document.getElementById('btn');
 
-
-let password = '';
-
 // LENGTH
 lengthElement.textContent = rangeElement.value;
 
@@ -17,59 +14,103 @@ const printLength = () => {
 };
 rangeElement.addEventListener('input', printLength);
 
-
 const upperCheckElement = document.getElementById('uppercase');
 const lowerCheckElement = document.getElementById('lowercase');
 const numbersCheckElement = document.getElementById('numbers');
 const symbolsCheckElement = document.getElementById('symbols');
 
-//UPPERCASE
-const addUppercase = event => {
-  upperCheckElement.checked === true
-  console.log(characters.toString()) 
-}
-upperCheckElement.addEventListener('input', addUppercase)
-
-//LOWERCASE
-const addLowercase = event => {
-  lowerCheckElement.checked === true
-  console.log(characters.toString()) 
-}
-lowerCheckElement.addEventListener('click', addLowercase)
-
-//NUMBERS
-const addNumbers = event => {
-  numbersCheckElement.checked === true
-  console.log(characters.toString()) 
-}
-numbersCheckElement.addEventListener('click', addNumbers)
-
-//SYMBOLS
-const addSymbols = event => {
-  symbolsCheckElement.checked === true
-  console.log(characters.toString()) 
-}
-symbolsCheckElement.addEventListener('click', addSymbols)
-
 //ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/
-const characters = [];
+/* const uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+const numbersCharacters = '0123456789';
+const symbolsCharacters = '!@#$%^&*()_+-={}[]:;<>,.?/'; */
+
+const passwordOptions = {
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  numbers: '0123456789',
+  symbols: '!@#$%^&*()_+-={}[]:;<>,.?/'
+};
+
+let characters = '';
+let password = '';
+
+//DESACTIVAR BOTÓN
+
+const getDisabledButton = () => {
+  //Esto sirve para desactivar el boton si no hay elementos en el string
+  btnElement.disabled = !characters.length;
+};
+
+//AÑADIR CARACTERES
+
+const getCharacters = event => {
+  //Otra opcion para desactivar el botón:
+  //if (checboxes === 0) { return; }
+
+  // Es igual if (even.target.id === 'uppercase')
+  /* if (upperCheckElement.checked) {
+    characters += uppercaseCharacters;
+  }
+  if (lowerCheckElement.checked) {
+    characters += lowercaseCharacters;
+  }
+  if (numbersCheckElement.checked) {
+    characters += numbersCharacters;
+  }
+  if (symbolsCheckElement.checked) {
+    characters += symbolsCharacters;
+  } */
+
+  characters = '';
+
+  // guarda en un 'array' los checked
+  const checkboxes = document.querySelectorAll('input:checked');
+  checkboxes.forEach(input => (characters += passwordOptions[input.id]));
+
+  getDisabledButton();
+};
+upperCheckElement.addEventListener('change', getCharacters);
+lowerCheckElement.addEventListener('change', getCharacters);
+numbersCheckElement.addEventListener('change', getCharacters);
+symbolsCheckElement.addEventListener('change', getCharacters);
 
 //PASSWORD
+
+const getPassword = () => {
+  /*   if (upperCheckElement.checked) {
+    characters += uppercaseCharacters;
+    } */
+
+  password = '';
+
+  for (let index = 0; index < rangeElement.value; index++) {
+    const randomNumber = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomNumber);
+  }
+
+  passwordElement.value = password;
+};
+btnElement.addEventListener('click', getPassword);
+
+//
+// MI VERSIÓN_____________________________________
+/* let password = '';
 const getPassword = () => {
 
-  if (upperCheckElement.checked === true) {
+  if (upperCheckElement.checked) {
     characters.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
   }
 
-  if (lowerCheckElement.checked === true) {
+  if (lowerCheckElement.checked) {
     characters.push('abcdefghijklmnopqrstuvwxyz')
   }
 
-  if (numbersCheckElement.checked === true) {
+  if (numbersCheckElement.checked) {
     characters.push('0123456789')
   }
 
-  if (symbolsCheckElement.checked === true) {
+  if (symbolsCheckElement.checked) {
     characters.push('!@#$%^&*()_+-={}[]:;<>,.?/')
   }
   console.log(characters)
@@ -86,4 +127,4 @@ const getPassword = () => {
   console.log(characters)
   passwordElement.value = password;
 };
-btnElement.addEventListener('click', getPassword);
+btnElement.addEventListener('click', getPassword); */
